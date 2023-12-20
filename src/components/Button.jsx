25f627cf04e6ecfc44b972c19b5729e12594e91a
Tai <5506726tai@gmail.com>
2023-12-20 15:31:05 +0700

    ex finish

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,3 +1,39 @@
 // Start coding here
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
 
-function Button() {}
+export function Button(props) {
+  if (props.type === "primary") {
+    return (
+      <button
+        css={css`
+          padding: 20px;
+          background-color: #0000ff;
+          font-size: 24px;
+          border: none;
+          border-radius: 5px;
+          text-align: center;
+          color: white;
+        `}
+      >
+        Button
+      </button>
+    );
+  } else if (props.type === "secondary")
+    return (
+      <button
+        css={css`
+          padding: 20px;
+          background-color: #87ceeb;
+          font-size: 24px;
+          border: none;
+          border-radius: 5px;
+          text-align: center;
+          color: white;
+        `}
+      >
+        Button
+      </button>
+    );
+  return <Button></Button>;
+}
